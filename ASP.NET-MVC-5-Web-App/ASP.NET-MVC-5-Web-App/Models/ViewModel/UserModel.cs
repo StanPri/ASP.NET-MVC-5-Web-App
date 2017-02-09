74cd67ceae2ea23ef92e040f1d31cee1d850a97f
StanPri <spribytkovsky@yahoo.com>
2017-02-08 16:30:12 -0800

    EF DB model and ViewModel added

---
+++ ASP.NET-MVC-5-Web-App/ASP.NET-MVC-5-Web-App/Models/ViewModel/UserModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC5RealWorld.Models.ViewModel
+{
+
+    public class UserSignUpView
+    {
+        [Key]
+        public int SYSUserID { get; set; }
+        public int LOOKUPRoleID { get; set; }
+        public string RoleName { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Login ID")]
+        public string LoginName { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+    }
+}
